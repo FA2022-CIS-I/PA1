f050d26ec1fba7e072335146af58cb6a39ce6d7b
jonat <jonathany330@gmail.com>
2022-09-29 00:19:38 -0400

    functionality added for parsing inputs

--- PROGRAMS/pointCloud.py
+++ PROGRAMS/pointCloud.py
@@ -13,9 +13,29 @@ class PointCloud:
 def extractFromFile(fpath):
     # Get Header
     header = pd.read_csv(fpath,header=None,nrows =1)
-    # Get File Name
+    # Get File Name    
     name = header.values[0][-1].split("-")[-1].split(".")[0]
+    frameInfo = header.values[0,0:-1]
     # Dictionary of Frames
-    nframes = {'calbody':1,}
+    nframes = {'calbody':1,'calReadings':frameInfo[-1],'empivot':frameInfo[-1],'optpivot':frameInfo[-1],'output':frameInfo[-1]}
+    # Acquire Data
+    frameData = pd.read_csv(fpath,header=None,names=["x","y","z"],skiprows=1)
+    
+    # Index Ranges
+    startPoints = getIncrementIndex(frameInfo)
+    
+    #Parsing Data
+    frame_clouds = []
+    startingPoint = 0
+    for itteration in nframes[name]:
+        
     
     
+def getIncrementIndex(frameInfo):
+    startPoints = []
+    start = 0
+    startPoints.append(0)
+    for index in frameInfo:
+        start = start + index
+        startPoints.append(start)
+    return startPoints
