f9d96b4c2d6ffdad10613d1d72979770d38cc4e2
jonat <jonathany330@gmail.com>
2022-09-28 22:30:21 -0400

    initial commit

---
+++ PROGRAMS/driver.py
@@ -0,0 +1,32 @@
+
+import sys
+
+from numpy import empty
+
+
+def main():
+
+    # Placeholders for files
+    calbody = None
+    calReadings = None
+    empivot = None
+    optpivot = None
+
+    # Check if appropriate amount of args
+    if (len(sys.argv) == 4):
+        # Assign
+        calBody = sys.argv[1]
+        calreadings = sys.argv[2]
+        empivot = sys.argv[3]
+        optpivot = sys.argv[4]
+    else:
+        print("Insufficent Arguments")
+
+def problems(calBody,calReadings,empivot,optpivot):
+    a
+    #TODO
+
+
+
+if __name__ == "__main__":
+    main()
